465e9b477df5b33547a9438e73bc49c4d1b0a3a5
haiaza <ajames1342@gmail.com>
2024-07-29 22:09:46 -0400

    new views

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const logger = require('morgan')
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = 3000
 //
-app.use(express.json())
+app.use(express.json()) //* parse the body and get back json
+app.use(express.urlencoded({ extended: true })) //* parsing the body and accepting urlencoded data
 app.use(logger('tiny'))
 mongoose.connect(MONGODB_URI)
 mongoose.connection.once('open', () => {
@@ -24,30 +25,70 @@ mongoose.connection.on('error', () => {
 
 // Create
 app.post('/notes', async (req, res) => {
+    req.body.isRead === 'on'  || req.body.isRead === true ?
+    req.body.isRead = true :
+    req.body.isRead = false
     try {
         const createdNote = await Note.create(req.body)
-        res.json(createdNote)
+        res.status(301).redirect(`/notes/${createdNote._id}`)
     } catch (error) {
         res.status(400).json({msg: error.message})
     }
 })
 
+app.get('/notes/new', (req, res) =>{
+    res.render('new.ejs')
+})
+
+
+// app.post('/notes', async (req, res) => {
+//     try {
+//         const createdNote = await Note.create(req.body)
+//         res.json(createdNote)
+//     } catch (error) {
+//         res.status(400).json({msg: error.message})
+//     }
+// })
+
+
 //Read
 //Index and Show
 
+// app.get('/notes', async (req, res) =>{
+//     try {
+//         const foundNotes = await Note.find({})
+//         res.json(foundNotes)
+//     } catch (error) {
+//         res.status(400).json({ msg: error.message })
+//     }
+// })
+//* Index
 app.get('/notes', async (req, res) =>{
     try {
-        const foundNotes = await Note.find({})
-        res.json(foundNotes)
+        const foundNotes = await Note.find()
+        res.render('index.ejs', {
+            notes : foundNotes
+        })
     } catch (error) {
-        res.status(400).json({ msg: error.message })
+        res.status(400).json({ msg: error.message})
     }
 })
 
+// app.get('/notes/:id', async (req, res) => {
+//     try {
+//         const foundNote = await Note.findOne({_id: req.params.id})
+//         res.json(foundNote)
+//     } catch (error) {
+//         res.status(400).json({ msg: error.message })
+//     }
+// })
+//* Show
 app.get('/notes/:id', async (req, res) => {
     try {
         const foundNote = await Note.findOne({_id: req.params.id})
-        res.json(foundNote)
+        res.render('show.ejs', {
+            note: foundNote
+        })
     } catch (error) {
         res.status(400).json({ msg: error.message })
     }
